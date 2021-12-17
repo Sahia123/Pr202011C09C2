b37b18d9ed1df0be1034d6cfd969243f345af2a5
Sahia123 <sahiatech123@gmail.com>
2021-12-17 20:19:18 +0500

    This is 1st commit

---
+++ CodeFirstAppraoch/Models/AptechDbContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirstAppraoch.Models
+{
+    public class AptechDbContext:DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public AptechDbContext():base("conn")
+        {
+
+        }
+    }
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        [ForeignKey("Teacher")]
+        public int TeacherId { get; set; }
+        public Teacher Teacher { get; set; }
+    }
+    public class Teacher
+    {
+        public int TeacherId { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public ICollection<Student> Students { get; set; }
+    }
+}
